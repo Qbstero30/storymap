fb1c13265cbc928fa702448cf6c4e772657cfe1c
Qbstero30 <149096912+Qbstero30@users.noreply.github.com>
2023-10-26 19:13:36 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1BCOhHRkm-nb55DkVfyR7XKG__TiU285Er_BrQbTIisA/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyCa75AxazfrH9SvgEfmJgFpHKsOxZgfk7A';
+var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
